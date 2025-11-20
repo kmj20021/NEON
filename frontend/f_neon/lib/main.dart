b633225b0a8570b221c1ab5b5d69050b257d0e7b
qkrgksql0123-ops <qkrgksql0123@gmail.com>
2025-11-20 21:43:21 +0900

    캘린더까지

--- frontend/f_neon/lib/main.dart
+++ frontend/f_neon/lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
-import 'login.dart'; // LoginPage 클래스가 있는 파일 import
-//import 'package:f_neon/accept.dart';
+import 'package:intl/date_symbol_data_local.dart'; // ← 반드시 추가!
+import 'login.dart'; // LoginPage 클래스가 있는 파일
+import 'main_page.dart'; // MainScreen 파일
 
-void main() {
+void main() async {
+  // ✅ Flutter 엔진 초기화 (비동기 호출 전 필수)
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // ✅ 한국어 날짜 포맷 초기화 (LocaleDataException 해결)
+  await initializeDateFormatting('ko_KR', null);
+
   runApp(const MyApp());
 }
 
@@ -12,10 +19,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Login Demo',
-      theme: ThemeData(primarySwatch: Colors.red),
-      home: const LoginPage(), // 여기에 LoginPage 연결!
-      debugShowCheckedModeBanner: false, // 상단 debug 배너 제거
+      title: '프로해빗',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        scaffoldBackgroundColor: Colors.grey[100],
+      ),
+      // ✅ 로그인 화면 혹은 메인 화면으로 연결
+      home: const MainScreen(), // 또는 const LoginPage()
     );
   }
 }
--- frontend/f_neon/lib/main_page.dart
+++ frontend/f_neon/lib/main_page.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  // 상태를 저장할 변수 (예시)
+  bool isAddedToRoutine = false;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[100],
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
@@ -36,86 +48,217 @@ class MainScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Container(
-        width: double.infinity,
-        height: 300,
-        color: Colors.white, // 전체 배경 흰색
-        padding: const EdgeInsets.all(16),
-        child: Center(
-          child: Container(
-            width: double.infinity,
-            padding: EdgeInsets.all(16),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.redAccent),
-              borderRadius: BorderRadius.circular(12),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // 아이콘 + 텍스트 줄
-                Row(
-                  children: [
-                    Icon(Icons.trending_up, color: Colors.redAccent),
-                    SizedBox(width: 8),
-                    Text(
-                      "추적 운동",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 14,
-                        color: Colors.black,
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: [
+            // 추천 운동 카드
+            Container(
+              width: double.infinity,
+              margin: const EdgeInsets.symmetric(vertical: 12),
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.redAccent),
+                borderRadius: BorderRadius.circular(12),
+                color: Colors.white,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // 추천 운동 헤더
+                  Row(
+                    children: [
+                      Icon(Icons.trending_up, color: Colors.redAccent),
+                      SizedBox(width: 8),
+                      Text(
+                        "추천 운동",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14,
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 7),
-
-                // 추가 내용 넣을 부분
-                Row(
-                  children: [
-                    Icon(
-                      Icons.access_time,
-                      color: Colors.redAccent,
-                      size: (20),
-                    ),
-                    SizedBox(width: 10),
-                    Text(
-                      "마지막 등 운동 후 5일 경과!",
-                      style: TextStyle(
+                    ],
+                  ),
+                  SizedBox(height: 7),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.access_time,
+                        size: 18,
                         color: Colors.redAccent,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 14,
                       ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 30),
-
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start, // 핵심 포인트
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(16),
-                      child: Image.asset(
-                        'assets/dead.jpg',
-                        width: 100,
-                        height: 100,
-                        fit: BoxFit.cover,
+                      SizedBox(width: 6),
+                      Text(
+                        "마지막 등 운동 후 5일 경과!",
+                        style: TextStyle(
+                          color: Colors.redAccent,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                    SizedBox(width: 12),
-                    Text(
-                      "데드리프트",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 15,
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  // 운동 상세
+                  Row(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(12),
+                        child: Image.asset(
+                          'assets/dead.jpg',
+                          width: 100,
+                          height: 100,
+                          fit: BoxFit.cover,
+                        ),
                       ),
+                      SizedBox(width: 12),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "데드리프트",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                            SizedBox(height: 4),
+                            Text(
+                              '전신 근력 발달에 효과적인 복합 운동',
+                              style: TextStyle(color: Colors.grey[700]),
+                            ),
+                            SizedBox(height: 10),
+                            Row(
+                              children: [
+                                _tag(
+                                  "등",
+                                  bgColor: Colors.grey[300],
+                                  textColor: Colors.grey[800],
+                                ),
+                                SizedBox(width: 8),
+                                _tag(
+                                  "강도: 고",
+                                  bgColor: Colors.red[100],
+                                  textColor: Colors.redAccent,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  // 버튼
+                  SizedBox(
+                    width: double.infinity,
+                    child: OutlinedButton.icon(
+                      onPressed: () {
+                        print("루틴에 추가하기 클릭됨!");
+                      },
+                      icon: Icon(Icons.add),
+                      label: Text("루틴에 추가하기"),
                     ),
-                  ],
-                ),
-              ],
+                  ),
+                ],
+              ),
             ),
-          ),
+            Container(
+              width: double.infinity,
+              margin: const EdgeInsets.symmetric(vertical: 12),
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(12),
+                color: Colors.white,
+              ),
+              child: Column(
+                children: [
+                  TableCalendar(
+                    locale: 'ko_KR',
+                    firstDay: DateTime.utc(2020, 1, 1),
+                    lastDay: DateTime.utc(2030, 12, 31),
+                    focusedDay: _focusedDay,
+                    calendarFormat: CalendarFormat.week,
+                    selectedDayPredicate: (day) {
+                      return isSameDay(_selectedDay, day);
+                    },
+                    onDaySelected: (selectedDay, focusedDay) {
+                      setState(() {
+                        _selectedDay = selectedDay;
+                        _focusedDay = focusedDay;
+                      });
+                    },
+                    headerStyle: HeaderStyle(
+                      formatButtonVisible: false,
+                      titleCentered: true,
+                    ),
+                    calendarStyle: CalendarStyle(
+                      todayDecoration: BoxDecoration(
+                        color: Colors.orangeAccent,
+                        shape: BoxShape.circle,
+                      ),
+                      selectedDecoration: BoxDecoration(
+                        color: Colors.redAccent,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: double.infinity,
+                    margin: const EdgeInsets.symmetric(vertical: 12),
+                    padding: const EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Color.fromARGB(255, 248, 221, 187),
+                      ),
+                      borderRadius: BorderRadius.circular(12),
+                      color: Color.fromARGB(255, 255, 247, 237),
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          "🔥 3일 연속 출석!!",
+                          style: TextStyle(
+                            color: Colors.redAccent,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "꾸준한 운동으로 목표를 달성해보세요",
+                          style: TextStyle(fontSize: 12),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
+      ),
+    );
+  }
+
+  Widget _tag(String text, {Color? bgColor, Color? textColor}) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+      decoration: BoxDecoration(
+        color: bgColor,
+        borderRadius: BorderRadius.circular(6),
+      ),
+      child: Text(text, style: TextStyle(fontSize: 12, color: textColor)),
+    );
+  }
+
+  Widget _calendarDay(String day, {bool selected = false}) {
+    return CircleAvatar(
+      radius: 14,
+      backgroundColor: selected ? Colors.redAccent : Colors.transparent,
+      child: Text(
+        day,
+        style: TextStyle(color: selected ? Colors.white : Colors.black),
       ),
     );
   }
